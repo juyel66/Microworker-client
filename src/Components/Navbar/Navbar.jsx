28d532af7e94ee74177193fb0e72100e22b38e59
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-06-02 15:43:15 +0600

    remove toggle

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import useAuth from "../Home/useAuth";
 import { LiaCoinsSolid } from "react-icons/lia";
 import useUserData from "../DashBoard/useUserData/useUserData";
 import { useEffect, useState } from "react";
+import { CgProfile } from "react-icons/cg";
 
 
 const Navbar = () => {
   const {user, logOut} = useAuth();
   const [workerData] = useUserData();
-  const [theme, setTheme] = useState(() => {
+  const [theme] = useState(() => {
     const storedTheme = localStorage.getItem('theme');
     return storedTheme || 'yellow';
   });
@@ -27,7 +28,7 @@ const Navbar = () => {
     <>
      <li><NavLink className={({ isActive }) => isActive? "bg-green-500 text-white btn-success text-bold text-[14px]": "text-[14px] font-bold lg:text-white text-black " } to="/" > Home</NavLink>
       </li>
-     <li><NavLink className={({ isActive }) => isActive? "bg-gray-500 text-white btn-success text-bold text-[14px]": "text-[14px] font-bold lg:text-white text-black " } to='https://www.youtube.com/watch?v=Dn_QYofxH34' target="_blank" >Watch Demo</NavLink>
+     <li><NavLink className={({ isActive }) => isActive? "bg-gray-500 text-white btn-success text-bold text-[14px]": "text-[14px] font-bold lg:text-white text-black " } to='https://www.facebook.com/share/v/19DsoQTAQ8/' target="_blank" >Watch Demo</NavLink>
       </li>
      
       <li><NavLink className={({ isActive }) => isActive? "bg-gray-500 text-white btn-success text-bold text-[14px]": "text-[14px] font-bold lg:text-white text-black " } to='/dashboard/welcome'  >Dashboard</NavLink>
@@ -41,10 +42,10 @@ const Navbar = () => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
 
-  const handleToggle = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-  };
+  // const handleToggle = () => {
+  //   const newTheme = theme === 'light' ? 'dark' : 'light';
+  //   setTheme(newTheme);
+  // };
 
 
     return (
@@ -93,13 +94,18 @@ const Navbar = () => {
    
 
       {
-        workerData && <img src={workerData?.photoURL} alt="" className="w-12 h-12 mr-2 rounded-full" />
+        user ? <img src={workerData?.photoURL} alt="" className="w-12 h-12 mr-2 border-2 rounded-full" /> 
+        : 
+            <p  >
+        <CgProfile  className="w-10 h-10"   />
+      </p>
       }
+  
      
 
     </div>
 
-    <div className="mr-2">
+    {/* <div className="mr-2">
 <label className="grid cursor-pointer place-items-center">
             <input
               onChange={handleToggle}
@@ -110,7 +116,7 @@ const Navbar = () => {
             <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
             <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
           </label>
-</div>
+</div> */}
    {
     user? 
     <a  onClick={handleLogout} className="hidden lg:flex btn btn-sm">Log Out</a> :
